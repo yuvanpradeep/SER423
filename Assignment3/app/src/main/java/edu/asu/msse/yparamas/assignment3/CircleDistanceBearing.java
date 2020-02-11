5a780d0efa9b3c7871739afe0508d21566ff1dfa
yuvanpradeep <yuvanpradeep594@gmail.com>
2020-02-10 18:07:38 -0700

    Renaming few attributes

--- Assignment3/app/src/main/java/edu/asu/msse/yparamas/assignment3/CircleDistanceBearing.java
+++ Assignment3/app/src/main/java/edu/asu/msse/yparamas/assignment3/CircleDistanceBearing.java
@@ -95,20 +95,19 @@ public class CircleDistanceBearing extends AppCompatActivity  implements  Adapte
 
     public double calculateGreatCircle(double latitude1, double latitude2,
                                        double longitude1, double longitude2) {
-        longitude1 = Math.toRadians(longitude1);
-        longitude2 = Math.toRadians(longitude2);
         latitude1 = Math.toRadians(latitude1);
         latitude2 = Math.toRadians(latitude2);
+        longitude1 = Math.toRadians(longitude1);
+        longitude2 = Math.toRadians(longitude2);
 
-
-        double dlon = longitude2 - longitude1;
-        double dlat = latitude2 - latitude1;
-        double a = Math.pow(Math.sin(dlat / 2), 2)
+        double distanlon = longitude2 - longitude1;
+        double distanlat = latitude2 - latitude1;
+        double a = Math.pow(Math.sin(distanlat / 2), 2)
                 + Math.cos(latitude1) * Math.cos(latitude2)
-                * Math.pow(Math.sin(dlon / 2),2);
+                * Math.pow(Math.sin(distanlon / 2),2);
 
         double calc = 2 * Math.asin(Math.sqrt(a));
-        double earthRad = 6371;
+        double earthRad = 3956.0;
 
         return(calc * earthRad);
     }
